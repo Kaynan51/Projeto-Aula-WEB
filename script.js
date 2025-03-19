6c489602d406ee95a0ebe3e8d7a3cf6a18ffe3f0
Kaynan <kaynan.castr@gmail.com>
2025-03-18 22:05:31 -0300

    Adicionando esqeuci a senha

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Função para o login
 function acionarBotao() {
     var textEmail = document.getElementById('email').value;
     var textPassword = document.getElementById('password').value;
@@ -7,26 +8,26 @@ function acionarBotao() {
     } else if (textPassword == "") {
         alert("Preencha o campo senha!");
     } else {
-        
         if (textEmail == "admin@admin.com" && textPassword == "admin") {
-            window.location.href = 'index.html';
+            window.location.href = 'index.html'; // Redireciona para a página inicial se for admin
         } else {
             alert("E-mail ou senha incorretos!");
         }
     }
 }
+
+// Função para o cadastro
 function acionarCadastro() {
     var textNome = document.getElementById('nome').value;
     var textEmail = document.getElementById('email').value;
     var textSenha = document.getElementById('senha').value;
     var textInteresses = document.getElementById('interesses').value;
     var termos = document.getElementById('termos').checked;
+    const confirmaSenha = document.getElementById("confirmaSenha").value;
 
     if (textNome == "Admin" && textEmail == "admin@admin.com" && textSenha == "admin") {
-        window.location.href = 'login.html';
+        window.location.href = 'login.html'; // Redireciona para o login caso o admin tente cadastrar
     }
-  
-
 
     if (textNome == "") {
         alert("Preencha o campo nome!");
@@ -38,9 +39,24 @@ function acionarCadastro() {
         alert("A senha precisa ter pelo menos 6 caracteres!");
     } else if (!termos) {
         alert("Você precisa aceitar os termos e condições.");
+    } else if (textSenha != confirmaSenha) {
+        alert("As senhas não coincidem. Tente novamente.");
+        return;
     } else {
-
         alert("Cadastro realizado com sucesso!");
-        window.location.href = 'login.html';
+        window.location.href = 'login.html'; // Redireciona para o login após cadastro
+    }
+}
+
+// Função para a página "Esqueci a Senha"
+function acionarRecuperacao() {
+    var textEmail = document.getElementById('email').value;
+
+    // Verifica se o e-mail é o admin
+    if (textEmail == "admin@admin.com") {
+        window.location.href = "login.html";
+    } else {
+        alert("Instruções de recuperação de senha enviadas para " + textEmail);
+        window.location.href = "login.html"; // Simula o envio e redireciona para login
     }
 }
